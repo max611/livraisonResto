77b3a7b6d3597e20f7950757199785266ed89668
Max <maxstonge@gmail.com>
2014-09-20 22:38:53 -0400

    Ajout des pages de modifications de compte et +

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,10 +2,11 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.mvc.Scope.Session;
 
 import java.util.*;
-import play.Logger;
 
+import play.Logger;
 import models.*;
 
 public class Application extends Controller 
@@ -18,8 +19,14 @@ public class Application extends Controller
     	User user = User.find("username", username).first();
     	Boolean login = user != null && user.password.equals(password);
     	Logger.info("Login: "+login);
-    	Scope.Session("username",user.username);
+    	//Scope.Session("username",user.username);
+    	
+		session.put("username", username);
+		Logger.info("test session = " + session.get("username"));
+
+
     	render(login,user);
+ 
     }
 
     public static void create(String username, String password, String email, String firstname, String lastname, int phonenumber) {
@@ -37,8 +44,9 @@ public class Application extends Controller
 		render(user);
     }
 
-        public static void update(String password, String firstname, String lastname, int phonenumber) {
-		User test = User.find("username", session("username")).first();
+        public static void update(String username, String password, String firstname, String lastname, int phonenumber) {
+        Logger.info("username dans la session = " + session.get("username"));
+        User test = User.find("username", session.get("username") ).first();
 
 		test.firstName = firstname;
 		test.lastName = lastname;
@@ -46,11 +54,32 @@ public class Application extends Controller
 		test.phonenumber = phonenumber;
     	
 		test.save();
-		render();
+		render(test);
     }
 
     public static void formulaire() {
 
         render();
     }
+
+
+    public static void logout() {
+    	session.clear();
+    	Logger.info("Session apres logout: "+ session);
+    		index();
+    }
+
+    public static void account(String username, String password, String firstname, String lastname, int phonenumber) {
+        Logger.info("username dans la session = " + session.get("username"));
+        User test = User.find("username", session.get("username") ).first();
+
+		test.firstName = firstname;
+		test.lastName = lastname;
+		test.password = password;
+		test.phonenumber = phonenumber;
+    	
+		test.save();
+		render(test);
+    }
+
 }
---
+++ app/models/Menu.java
@@ -0,0 +1,11 @@
+package models;
+
+import play.db.jpa.*;
+import javax.persistence.*;
+import play.data.validation.*;
+
+@Entity
+public class Menu extends Model
+{
+
+}
