cb5b6af579a01e212d6c6630d5b920c1e5829bcb
temgoua <thrilogies@gmail.com>
2014-09-25 11:03:29 -0400

    ajout des tests dans le repertoire test voir #1

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -6,8 +6,18 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
+    /*
     public void aVeryImportantThingToTest() {
         assertEquals(2, 1 + 1);
     }
-
+*/
+    public void createAndTestnewUser(){
+    	// create a new user and save it
+    	new User("john@yahoo.fr", "avion123","john").save();
+    	
+    	User john = User.find("byEmail", "john@yahoo.fr").first();
+    	//Test
+    	assertNotNull(john);
+    	assertEquals("john",john.fullname);
+    }
 }
